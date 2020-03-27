7503f797e416d675b08a5796ca27d11ead3d000b
Jon Rice <revyolution1120@gmail.com>
2020-03-26 20:15:18 -0700

    Can cycle through questions and answers

--- js/oop.js
+++ js/oop.js
@@ -11,12 +11,10 @@ function Question_oop(id, name, question, answer, resource){
 
 var whatIsOOP = new Question_oop(1, "What_Is_OOP", "What does OOP stand for?", "Object Oriented Programing", "N/A");
 var whyOOP = new Question_oop(2, "Why_OOP", "Why use OOP in our programing?", "Better manageability of code base through encapsulation allowing for changing the underlying code but keep the same function", "N/A");
+var principles = new Question_oop(3, "Principles" ,"What are the four main principles of OOP?", "Encapsulation, Abstraction, Polymorphism, Inheritance.", "N/A");
 console.log(oopQuestions);
 
 // jQUery actions for OOP Questions
-// TODO: Have setup function
-// TODO: Have next Question function
-// TODO: Grab correct object from array
 $(function(){
     var i = 0;
     var oopStart = oopQuestions[i];
@@ -27,25 +25,25 @@ $(function(){
         .append("<a class = nav-link id = OOP_Answer_Show href=#>Show Answer</a><br>")
         .append("<a class = nav-link id = Next_Question href=#>Next Question</a>");
         $("#question").text(oopStart.question);
-
+        $("#answer").text(oopStart.answer).hide();
     }
 
     var nextQuestion = $(".start").find("#question");
-    console.log(nextQuestion);
+    var nextAnswer = $(".start").find("#answer");
     function populateQuestion(){
-        console.log("Hit");
         i = (i + 1) % oopQuestions.length;
-        console.log(i);
         var next = oopQuestions[i];
         nextQuestion.replaceWith("<p id = question></p>")
         $("#question").text(next.question);
+        nextAnswer.replaceWith("<p id = answer></p>")
+        $("#answer").text(next.answer);
         };
 
     $("#OOP").on("click", populateOOP);
 
 
     $(".start").on("click", "#OOP_Answer_Show", function(){
-        $("#answer").text(oopStart.answer).show();
+        $("#answer").show();
         $("#OOP_Answer_Show").replaceWith("<a class = nav-link id = OOP_Answer_Hide>Hide Answer</a>");
     });
 
