7db363050586310d4915cd78277d910ae70e9d7c
Jon Rice <revyolution1120@gmail.com>
2020-03-27 14:35:54 -0700

    Working random question and added in sql js

--- js/oop.js
+++ js/oop.js
@@ -12,8 +12,14 @@ function Question_oop(id, name, question, answer, resource){
 var whatIsOOP = new Question_oop(1, "What_Is_OOP", "What does OOP stand for?", "Object Oriented Programing", "N/A");
 var whyOOP = new Question_oop(2, "Why_OOP", "Why use OOP in our programing?", "Better manageability of code base through encapsulation allowing for changing the underlying code but keep the same function", "N/A");
 var principles = new Question_oop(3, "Principles" ,"What are the four main principles of OOP?", "Encapsulation, Abstraction, Polymorphism, Inheritance.", "N/A");
+var encap = new Question_oop(4, "Encapsulation", "Describe Encapsulation", "BLAH BLAH", "N/A");
+var abstract = new Question_oop(5, "Abstraction", "Describe Abstraction", "BLAH BLAH", "N/A");
+var poly = new Question_oop(6, "Polymorphism", "Describe Polymorphism", "BLAH BLAH", "N/A");
+var inheritance = new Question_oop(7, "Inheritance", "Describe Inheritance", "BLAH BLAH", "N/A");
+
 console.log(oopQuestions);
 
+// TODO: Prevent build up of <p> tags when OOP link clicked
 // jQUery actions for OOP Questions
 $(function(){
     var i = 0;
@@ -23,14 +29,15 @@ $(function(){
         $(".start p").replaceWith("<p>OOP Questions</p>");
         $(".start").append("<p id = question></p> <p id = answer></p>")
         .append("<a class = nav-link id = OOP_Answer_Show href=#>Show Answer</a><br>")
-        .append("<a class = nav-link id = Next_Question href=#>Next Question</a>");
+        .append("<a class = nav-link id = Next_Question href=#>Next Question</a><br>")
+        .append("<a class = nav-link id = Rando href=#>Random Question</a><br>")
         $("#question").text(oopStart.question);
         $("#answer").text(oopStart.answer).hide();
     }
 
     var nextQuestion = $(".start").find("#question");
     var nextAnswer = $(".start").find("#answer");
-    function populateQuestion(){
+    function populateNextQuestion(){
         i = (i + 1) % oopQuestions.length;
         var next = oopQuestions[i];
         nextQuestion.replaceWith("<p id = question></p>")
@@ -39,8 +46,18 @@ $(function(){
         $("#answer").text(next.answer);
         };
 
-    $("#OOP").on("click", populateOOP);
+    function populateRandomQuestion(){
+        i = Math.floor(Math.random() * oopQuestions.length);
+        console.log(`The value of i is ${i}`)
+        var randomQuestion = oopQuestions[i];
+        nextQuestion.replaceWith("<p id = question></p>")
+        $("#question").text(randomQuestion.question);
+        nextAnswer.replaceWith("<p id = answer></p>")
+        $("#answer").text(randomQuestion.answer);
 
+    }
+
+    $("#OOP").on("click", populateOOP);
 
     $(".start").on("click", "#OOP_Answer_Show", function(){
         $("#answer").show();
@@ -52,5 +69,7 @@ $(function(){
         $("#OOP_Answer_Hide").replaceWith("<a class = nav-link id = OOP_Answer_Show>Show Answer</a>");
     });
 
-    $(".start").on("click", "#Next_Question", populateQuestion);
+    $(".start").on("click", "#Next_Question", populateNextQuestion);
+
+    $(".start").on("click", "#Rando", populateRandomQuestion);
 });
---
+++ js/sql.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#SQL").on("click", function(){
+        $(".start p").replaceWith("<p>SQL Hit</p>");
+    })
+})
