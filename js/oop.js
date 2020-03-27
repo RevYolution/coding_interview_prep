0b4685dde24c5f5990efb114d8c42539ab7cd6fa
Jon Rice <revyolution1120@gmail.com>
2020-03-26 19:04:37 -0700

    Can show question and show/hide answer

--- js/oop.js
+++ js/oop.js
@@ -10,13 +10,28 @@ function Question_oop(id, name, question, answer, resource){
 }
 
 var whatIsOOP = new Question_oop(1, "What_Is_OOP", "What does OOP stand for?", "Object Oriented Programing", "N/A");
-
+var whyOOP = new Question_oop(2, "Why_OOP", "Why use OOP in our programing?", "Better manageability of code base through encapsulation allowing for changing the underlying code but keep the same function", "N/A");
+console.log(oopQuestions);
 
 // jQUery actions for OOP Questions
 // TODO: switch to replaceWith()
 $(function(){
+    var oopStart = oopQuestions[0];
+
     $("#OOP").on("click", function(){
-        $(".start").append("<p>OOP Questions</p>").append("<p>`${oopQuestions[0].question}`</p>");
+        $(".start p").replaceWith("<p>OOP Questions</p>");
+        $(".start").append("<p id = question></p><p id = answer display = hidden></p>").append("<a class = nav-link id = OOP_Answer_Show href=#>Show Answer</a>");
+        $("#question").text(oopStart.question);
         
+    });
+
+
+    $(".start").on("click", "#OOP_Answer_Show", function(){
+        $("#answer").text(oopStart.answer).show();
+        $("#OOP_Answer_Show").replaceWith("<a class = nav-link id = OOP_Answer_Hide>Hide Answer</a>");
     })
-})
+    $(".start").on("click", "#OOP_Answer_Hide", function(){
+        $("#answer").hide();
+        $("#OOP_Answer_Hide").replaceWith("<a class = nav-link id = OOP_Answer_Show>Show Answer</a>");
+    })
+});
